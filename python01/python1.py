4d9cb1de57d74591b611d66c8e8846041b45e4ac
Anisur369 <anisurrahman57136@gmail.com>
2025-11-27 08:25:08 +0600

    commit

---
+++ python01/python1.py
@@ -0,0 +1,9 @@
+x=float(input("Please enter your first number: "))
+y=float(input("Please enter your second number: "))
+A=x+y
+print("The sum of",x,"and",y,"is =",A)
+
+# output:
+# Please enter your first number: 10
+# Please enter your second number: 20
+# The sum of 10.0 and 20.0 is = 30.0
