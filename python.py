b6ee180a365d5a5cc98e496376de074298bc4431
Anisur369 <anisurrahman57136@gmail.com>
2025-12-04 09:55:55 +0600

    application

--- python.py
+++ python.py
@@ -1,3 +1,15 @@
-# Tuple Repetition
-a=(5,12)
-print(a*2)
+F=float(input("Enter the value of Fahrenheit: "))
+C=(F-32)*5/9
+print("The value of Celsius is",C)
+
+# output:
+# Enter the value of Fahrenheit: 100
+# The value of Celsius is 37.77777777777778
+
+C=float(input("Enter the value of Celsius: "))
+F=(C*9/5)+32
+print("The value of Fahrenheit is",F)
+
+# output:
+# Enter the value of Celsius: 100
+# The value of Fahrenheit is 212
